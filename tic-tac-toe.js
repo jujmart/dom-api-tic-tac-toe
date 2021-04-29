205237396467b60dfc0050540702b72713a4bc85
Nico Pierson <nicogpt@gmail.com>
2021-04-29 16:00:19 -0700

    added giveup button

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,6 +6,7 @@ window.addEventListener("DOMContentLoaded", event => {
     let oMoves = [];
     let header = document.getElementById("game-status");
     let winner = false;
+    const giveUp = document.getElementById("give-up");
 
     const isItWinning = (arr) => {
         for (let j = 0; j < winningCombos.length; j++) {
@@ -16,6 +17,23 @@ window.addEventListener("DOMContentLoaded", event => {
         } 
         return false;
     }
+
+    const printWinner = (moves, player) => {
+        if (isItWinning(moves)) {
+            header.innerText = `Winner: ${player}`;
+            winner = true;
+        }
+    };
+
+    giveUp.addEventListener("click", (event) => {
+        if (count % 2 === 1) {
+            header.innerText = `Winner: X`;
+        } else {
+            header.innerText = `Winner: O`;
+        }
+        giveUp.disabled = true;
+        winner = true;
+    });
     
     board.addEventListener("click", event => {
         if (!winner) {
@@ -24,23 +42,18 @@ window.addEventListener("DOMContentLoaded", event => {
                 if (count % 2 === 1) {
                     event.target.innerHTML = `<img id="stop" class="X" src="https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg" alt="X">`;
                     xMoves.push(event.target.id.slice(-1));
-                    console.log(xMoves);
-                    if (isItWinning(xMoves)) {
-                        header.innerText = "Winner: X"
-                        winner = true;
-                    }
+                    printWinner(xMoves, "X");
+
                 } else {
                     event.target.innerHTML = `<img id="stop" class="O" src="https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg" alt="O">`;
                     oMoves.push(event.target.id.slice(-1));
-                    console.log(oMoves);
-                    if (isItWinning(oMoves)) {
-                        header.innerText = "Winner: O"
-                        winner = true;
-                    }
+                    // console.log(oMoves);
+                    printWinner(oMoves, "O");
                 }
             }
         }
     });
+
     let newGameButton = document.querySelector("button");
     newGameButton.addEventListener("click", event => {
         location.reload();
